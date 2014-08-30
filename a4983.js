18baf5b5f9c22e6c362582cde3706ea36d4f9b2f
Nelson Neves <nelson.s.neves@gmail.com>
2014-08-31 00:11:16 +0100

    Adding Event Emmiter to auto trigger Step()

--- a4983.js
+++ a4983.js
@@ -1,9 +1,11 @@
-var Gpio = require('onoff').Gpio;  
-//var Gpio = require('./emulate-onoff.js').Gpio;
+//var Gpio = require('onoff').Gpio;  
+var Gpio = require('./emulate-onoff.js').Gpio;
 
 var sleep = require('sleep');
 
+//------------------------------------------------------------------
 // setting constants values
+//------------------------------------------------------------------
 var HIGH = 1;
 var LOW = 0;
 var ON = 1;
@@ -15,13 +17,36 @@ var CounterClockwise = 0;
 
 var fMotorEnable = 0;		// flag to Enable/Disable Step Motor, 0 => Disable, 1 => Enable
 var fMotorDirection = 0;	// flag for the Step Motor direction, 0 => Normal, 1 => Inverted
-var kDelay = 1;			// delay constant in mili-seconds (Step Motor Speed)
+var kDelay = 1000000;		// delay constant in mili-seconds (Step Motor Speed)
 
-var istep = 0;
-var itimer = 50;
-var icounter = 0;
+var util = require('util');
+var eventemitter = require('events').EventEmitter;
 
+//------------------------------------------------------------------
+// Event Emitter
+//------------------------------------------------------------------
+var EvntClass = function() {
+
+	//var eventemit = EVTClass();
+	if(!(this instanceof arguments.callee)) {
+		console.log("Create EvntClass and return object!");
+		return new arguments.callee();
+	}
+	console.log("EVTClass object.");
+}
+util.inherits(EvntClass, eventemitter);
+
+EvntClass.prototype.emmitStep = function () {
+	console.log("Event Triggered: Step");
+	step();
+}
+
+// Initialize Event Emmiter object
+var evnt = EvntClass();
+
+//------------------------------------------------------------------
 // mapping default values of GPIOs
+//------------------------------------------------------------------
 var led1 = new Gpio(48, 'out');
 
 var Dir = new Gpio(48, 'out');		// direction of movement
@@ -32,6 +57,8 @@ var nEnable = new Gpio(7, 'out');	// must be LOW to make it work
 //var MS3 = new Gpio(0, 'out');		// (optional) microstep mode selectors
 //var MS2 = new Gpio(0, 'out');
 //var MS1 = new Gpio(0, 'out');
+
+//------------------------------------------------------------------
 
 function initialize () {
 
@@ -60,49 +87,17 @@ function initialize () {
   //wait(0.5);
 };
 
-function autostep_x() {
-/*
-	if(Dir != f_motor_direction)
-	Dir = f_motor_direction;
-
-	Step = HIGH;
-	wait(k_delay/2);
-
-	Step = LOW;
-	wait(k_delay/2);
-*/
-
-  if(istep === 0) {
-    Step.writeSync(HIGH);
-    istep = 1;
-  }
-  else {
-    Step.writeSync(LOW);
-    istep = 0;
-  }
-
-/*
-  icounter++;
-  if(icounter % 50 === 0 && itimer > 0) {
-  	itimer -= 10;
-  	if(itimer <= 0)
-  		itimer = 1;
-  }
-*/
-  setTimeout(autostep, itimer); 
-};
-
-function autostep() {
+function step() {
   
-  for(var i=0; i<999999999; ++i) {
     Step.writeSync(HIGH);
-    sleep.usleep(itimer);
+    sleep.usleep(kDelay);
     Step.writeSync(LOW);
-    sleep.usleep(itimer);
-  }
+    sleep.usleep(kDelay);
+
+    evnt.emmitStep();
 };
 
 module.exports = {
 	initialize : initialize,
-	autostep: autostep
+	step: step
 };
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 var stepmotor = require('./a4983.js');
 
 stepmotor.initialize();
-stepmotor.autostep();
+stepmotor.step();
